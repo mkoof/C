5c4f04c6a9c8b73529144b4232c1b17addcd71b3
mkoof <mkoo4@outlook.com>
2022-07-29 15:30:39 +0800

    add debug info

--- src/lib.rs
+++ src/lib.rs
@@ -10,18 +10,38 @@ use prog::{BinProgram, Program};
 use runner::Runner;
 use token::Code;
 
+const DEBUG: bool = true;
+
 pub fn compile(code: &str) -> Result<BinProgram, CompileError> {
-    let code = Code::new(code);
-    let seq = code.lex()?;
-    let ast = AST::parse(&seq)?;
-    let prog = Program::new(&ast)?;
-    Ok(prog.to_bin())
+    fn _compile(code: &str) -> Result<BinProgram, CompileError> {
+        let code = Code::new(code);
+        let seq = code.lex()?;
+        let ast = AST::parse(&seq)?;
+        let prog = Program::new(&ast)?;
+        Ok(prog.to_bin())
+    }
+    let res = _compile(code);
+    if DEBUG {
+        if let Err(e) = res.as_ref() {
+            println!("{:?}", e);
+        }
+    }
+    res
 }
 
 pub fn run(prog: &BinProgram) -> Result<(), RuntimeError> {
-    let mut runner = Runner::new();
-    runner.load(&prog)?;
-    runner.run()
+    fn _run(prog: &BinProgram) -> Result<(), RuntimeError> {
+        let mut runner = Runner::new();
+        runner.load(&prog)?;
+        runner.run()
+    }
+    let res = _run(prog);
+    if DEBUG {
+        if let Err(e) = res.as_ref() {
+            println!("{:?}", e);
+        }
+    }
+    res
 }
 
 /// return None when load error
